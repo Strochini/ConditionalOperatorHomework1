d2f51bfe65c25f65937c201efe795c431889ed3f
Strochini <0.618@bk.ru>
2022-11-12 23:47:21 +0300

    Conditional operator HW 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age >= 18)
+        {
+            System.out.println("Поздравляем вы совершеннолетний"); }
+
+            if (age < 18)
+            {
+                System.out.println("К сожалению вы еще не достигли совершеннолетия и вам придется немного подождать");
+            }
+
+        }
+}
