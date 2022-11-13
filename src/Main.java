69516208544c4efda7e546a8d85de57cd4978c1c
Strochini <0.618@bk.ru>
2022-11-13 22:07:00 +0300

    Conditional operator HW 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 19;
-        if (age >= 18)
+        int driverAge = 19;
+
+        if (driverAge >= 18)
         {
             System.out.println("Поздравляем вы совершеннолетний"); }
 
-            if (age < 18)
+            if (driverAge < 18)
             {
                 System.out.println("К сожалению вы еще не достигли совершеннолетия и вам придется немного подождать");
+            }
+
+
+            int educationAge = 50;
+
+            if (educationAge >= 7)
+                if (educationAge < 18) {
+                System.out.println("Школьник");
+            }
+            if (educationAge >= 18)
+            if (educationAge < 24){
+                System.out.println("Студент");
+            }
+            if (educationAge >= 24) {
+                System.out.println("Уже работает");
+            }
+
+
+            int totalSpace = 102;
+            int seatPlace = 60;
+            int standPlace = totalSpace - seatPlace;
+            int humans = 101;
+
+            if (humans <= seatPlace) {
+                System.out.println("Сидячих мест осталось " + (seatPlace - humans));
+            }
+            if (humans > seatPlace) {
+                System.out.println("Сидячих мест не осталось");
+            }
+            if (humans < seatPlace)
+                if (humans < totalSpace) {
+                    System.out.println("Стоячих мест осталось " + standPlace);
+                }
+            if (humans > seatPlace)
+                if (humans < totalSpace) {
+                System.out.println("Стоячих мест занаято " + (humans - seatPlace) + " из 42");
+            }
+            if (humans >= totalSpace) {
+                System.out.println("Стоячих мест не осталось");
+                System.out.println("Вагон заполнен");
             }
 
         }
